69523dceb3f91023972fcb94ef686d5805fbd360
zhang <2268742924@qq.com>
2022-11-03 18:36:25 +0800

    evaluate

--- evaluate.py
+++ evaluate.py
@@ -3,8 +3,10 @@ import torch
 import numpy as np
 import gym
 import pygame as pg
+import os, sys
 
 from hyperparameters import get_hyperparameters
+from wrappers import wrap_deepmind, wrap_pytorch, make_atari
 
 def parse_input():
     parser = argparse.ArgumentParser()
@@ -17,7 +19,6 @@ def run(env, model):
     state, _ = env.reset()
     while True:
         env.render()
-
         action = model.act(state, 0)
         state, _, terminated, _, _ = env.step(action)
         if terminated or pg.key.get_pressed()[pg.K_ESCAPE]:
@@ -29,9 +30,27 @@ def main():
     if game == "cartpole":
         env = gym.make("CartPole-v1", render_mode='human')
         model = Model(env.observation_space.shape[0], env.action_space.n)
-        model.load_state_dict(torch.load(f"results/{dqn_type}_{game}/model.pt"))
-
-        run(env, model)
+        if os.path.exists(f"results/{dqn_type}_{game}/model.pt"):
+            model.load_state_dict(torch.load(f"results/{dqn_type}_{game}/model.pt"))
+            run(env, model)
+        else:
+            print("model not trained", file=sys.stderr)
+    elif game == "breakout":
+        env = wrap_pytorch(wrap_deepmind(make_atari("BreakoutNoFrameskip-v4", render_mode='human')))
+        model = Model(env.observation_space.shape, env.action_space.n)
+        if os.path.exists(f"results/{dqn_type}_{game}/model.pt"):
+            model.load_state_dict(torch.load(f"results/{dqn_type}_{game}/model.pt"))
+            run(env, model)
+        else:
+            print("model not trained", file=sys.stderr)
+    elif game == "pong":
+        env = wrap_pytorch(wrap_deepmind(make_atari("PongNoFrameskip-v4", render_mode='human')))
+        model = Model(env.observation_space.shape, env.action_space.n)
+        if os.path.exists(f"results/{dqn_type}_{game}/model.pt"):
+            model.load_state_dict(torch.load(f"results/{dqn_type}_{game}/model.pt"))
+            run(env, model)
+        else:
+            print("model not trained", file=sys.stderr)
 
 if __name__ == "__main__":
     main()
